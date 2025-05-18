4a480e655e1df0cfaef1f2b98653056c0af8de2f
Sushant Aryal <sushantaryal05@gmail.com>
2025-05-18 10:48:03 -0500

    main text layout and loop array

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+const words = [
+    {text: 'Ideas', imgPath: '/images/ideas.svg'},
+    {text: 'Concepts', imgPath: '/images/concepts.svg'},
+    {text: 'Code', imgPath: '/images/code.svg'},
+]
 const Hero = () => {
     return (
         <section id = "hero" className="relative overflow-hidden">
@@ -7,7 +12,19 @@ const Hero = () => {
                 <img src = "images/bg.png" alt = "background"/>
             </div>
 
-            
+            <div className ="hero-layout">
+                {/* Left ma hero content */}
+                <header className = "flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+                <div className = "flex flex-col gap-7">
+                    <div className = "hero-text">
+                        <h1>Shaping Designs</h1>
+                        <h1>Into Real Projects</h1>
+                        <h1>That Deliver Results</h1>
+                    </div>
+                </div>
+                </header>
+                {/* right ma 3d model */}
+            </div>
         </section>
     )
 }
